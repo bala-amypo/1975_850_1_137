79578587a9009aa7dd87cb00b5dc605a9e63e2de
bala-amypo <bala@amypo.in>
2025-12-22 01:03:46 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,6 @@
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-}
+spring.datasource.url=jdbc:mysql://localhost:3306/demo_db
+spring.datasource.username=root
+spring.datasource.password=Amypo
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+server.port=8080
--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -2,18 +2,12 @@ package com.example.demo.servlet;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-
 import java.io.IOException;
 
 @WebServlet("/simple-status")
 public class SimpleStatusServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp)
-            throws IOException {
-
-        resp.setContentType("text/plain");
-        resp.getWriter().write("Application is running successfully");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/plain");
+        res.getWriter().write("SaaS User Role Permission Manager is running");
     }
 }
