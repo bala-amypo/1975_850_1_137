794cf098b607427e8995b1e1d12095e299e4df22
bala-amypo <bala@amypo.in>
2025-12-23 04:16:19 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,8 +18,10 @@ public class OpenApiConfig {
                         .title("SaaS User Role Permission Manager API")
                         .version("1.0")
                         .description("API for managing users, roles and permissions"))
-                        .servers(List.of(
-                            new Server().url("https://9276.408procr.amypo.ai/")
-                        ));
+                .servers(List.of(
+                        new Server()
+                                .url("https://9276.408procr.amypo.ai/")
+                                .description("Production Server")
+                ));
     }
 }
