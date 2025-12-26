322162be3b5cd7c79c09b213566efd7949c512d2
bala-amypo <bala@amypo.in>
2025-12-26 16:58:02 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,29 +1,21 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("SaaS User Role Permission Manager API")
-                        .description("A comprehensive system for user authentication, role and permission management with fine-grained access control and RBAC")
-                        .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+    public OpenAPI customOpenAPI() {
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://9276.408procr.amypo.ai/"); 
+        httpsServer.setDescription("College Portal Server");
+
+        return new OpenAPI().servers(List.of(httpsServer));
     }
 }
