a1912abfab2ce704812188648ef0a68f13cff178
bala-amypo <bala@amypo.in>
2025-12-26 17:39:16 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 @WebServlet("/simple-status")
 public class SimpleStatusServlet extends HttpServlet {
 
+    
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
-        resp.setStatus(200);
+        resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("SaaS User Role Permission Manager is running");
     }
 }
